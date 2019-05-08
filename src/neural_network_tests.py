16576013b026744e499520cad4640701e47e91d4
felixPicard <felix.picard.bld@gmail.com>
2019-05-08 12:54:01 +0200

    CNN works, does not overload the RAM. I must learn how to teach it in several batches.

--- src/neural_network_tests.py
+++ src/neural_network_tests.py
@@ -12,7 +12,7 @@ import numpy as np
 def build_network():
     model = Sequential()
 
-    model.add(Convolution2D(filters=64, kernel_size=[3, 3], input_shape=(150, 300, 1), padding='same', activation='relu'))
+    model.add(Convolution2D(filters=64, kernel_size=[3, 3], input_shape=(75, 150, 3), padding='same', activation='relu'))
 
     model.add(Convolution2D(filters=64, kernel_size=[3, 3], activation='relu'))
 
@@ -29,86 +29,74 @@ def build_network():
     return model
 
 
-def get_images(value):
+def get_images(nb_of_pos, pos_path, nb_of_neg, neg_path):
 
-    file = open("../files/data_samples/" + value + ".txt", 'r')
+    pos_file = open("../files/data_samples/" + pos_path + ".txt", 'r')
+    neg_file = open("../files/data_samples/" + neg_path + ".txt", 'r')
 
-    paths = []
-    for line in file:
-        paths.append(line)
+    pos_maximum = 0
+    neg_maximum = 0
 
-    nb_of_images_to_read = 20 #len(paths)
+    pos_paths = []
+    neg_paths = []
 
-    out_images = np.zeros((nb_of_images_to_read, 150, 300, 1))
+    for line in pos_file:
+        pos_maximum += 1
+        pos_paths.append("../files/data_samples/" + line[2:-1])
+        if pos_maximum > nb_of_pos:
+            break
 
-    for i in range(nb_of_images_to_read):
-        out_images[i] = cv.imread(paths[i], 0)
+    for line in neg_file:
+        neg_maximum += 1
+        neg_paths.append("../files/data_samples/" + line[2:-1])
+        if neg_maximum > nb_of_neg:
+            break
+
+    if nb_of_pos > pos_maximum or nb_of_neg > neg_maximum:
+        exit(-1)
+
+    nb_of_images_to_read = nb_of_neg + nb_of_pos
+
+    out_images = np.zeros((nb_of_images_to_read, 75, 150, 3))
+
+    for i in range(nb_of_pos):
+        img_temp = cv.imread(pos_paths[i])
+        out_images[i] = cv.resize(img_temp, (150, 75))
+
+    for i in range(nb_of_neg):
+        img_temp = cv.imread(neg_paths[i])
+        out_images[i+nb_of_pos] = cv.resize(img_temp, (150, 75))
+
 
     return out_images
 
 
-model = build_network()
-qtt = 20
+def get_labels(nb_of_positives, nb_of_negatives):
 
-samples = np.ones((qtt, 150, 300, 1))
-samples = np.concatenate([samples, np.zeros((qtt, 150, 300, 1))])
+    labels = [1] * nb_of_positives
 
-grades = [1] * qtt
-for i in range(qtt):
-    grades.append(0)
+    for i in range(nb_of_negatives):
+        labels.append(0)
 
-model.fit(samples, grades)
+    return labels
 
-"""
+
 model = build_network()
 
-print("\nLoading database...\n")
-print("positive images...")
-samples = get_images("positives")
-print("positive images done !")
-nb_of_positives = len(samples)
-print("negatives images...")
-samples = np.concatenate([samples, get_images("negatives")])
-print("negative images done !")
+nb_of_positives = 200
+nb_of_negatives = 200
+nb_of_samples = nb_of_negatives + nb_of_positives
 
-print("\n")
+print("Loading dataset...")
 
-nb_of_samples = len(samples)
-nb_of_negatives = nb_of_samples - nb_of_positives
+samples = get_images(200, "positives", 200, "negatives")
 
 print(nb_of_positives, "positives, ", nb_of_negatives, "negatives, ", nb_of_samples, "total")
 
-labels = [1] * nb_of_positives
-for i in range(nb_of_negatives):
-    labels.append(0)
+labels = get_labels(nb_of_positives, nb_of_negatives)
 
 #model = load_model("../files/neural_networks/jooj")
 
-positives_to_train = int(nb_of_positives * 0.9)
-negatives_to_train = int(nb_of_negatives * 0.9)
-
-# TRAINING
-print("Network will be trained on ", positives_to_train + negatives_to_train, "samples.")
-
-training_samples = np.concatenate([samples[:positives_to_train], samples[nb_of_positives:nb_of_positives+negatives_to_train]])
-training_labels = np.concatenate([labels[:positives_to_train], labels[nb_of_positives:nb_of_positives+negatives_to_train]])
-
-testing_samples = np.concatenate([samples[positives_to_train:nb_of_positives], samples[nb_of_positives+negatives_to_train:]])
-testing_labels = np.concatenate([labels[positives_to_train:nb_of_positives], labels[nb_of_positives+negatives_to_train:]])
-
-model.fit(training_samples,training_labels)
+model.fit(samples,labels, shuffle=True)
 
 print("Network trained.")
-
-answer = model.predict(testing_samples)
-
-print(labels)
-print(answer)
-#print("we wnated")
-#print(testing_labels)
-
-error = sum( abs(max(answer[i])-testing_labels[i]) for i in range(len(answer)) )
-
-
-print("Erreur de : ", float(error)/float(nb_of_samples-(positives_to_train + negatives_to_train))*100, "%")
-"""
